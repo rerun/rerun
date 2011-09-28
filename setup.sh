40f1bf92960b134c6e06852cdb3c8aea23144998
Lee Thompson <lthompson@hp.com>
2011-09-27 22:05:03 -0500

    autoconf seems to be working now

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# RUN ME after pulling the code from git!
+if [ "clean" == "$1" ]; then
+  make distclean
+  chmod -R u+w rerun-*
+  rm -rf tmp rerun-* autom4te.cache config
+  rm INSTALL Makefile.in aclocal.m4 configure
+else
+  autoreconf --install
+  ./configure --prefix=/opt/junk
+  mkdir tmp
+  make DESTDIR=./tmp install
+  make distcheck
+fi
