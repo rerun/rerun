563fd8be68308ac4d25de5a6b354a731a6c727eb
Alex Honor <alexhonor@gmail.com>
2012-09-04 16:01:10 -0700

    added error message if function library can't be sourced.

--- modules/stubbs/templates/default.sh
+++ modules/stubbs/templates/default.sh
@@ -10,7 +10,7 @@
 #
 
 # Read module function library
-source $RERUN_MODULES/@MODULE@/lib/functions.sh || exit 1 ;
+source $RERUN_MODULES/@MODULE@/lib/functions.sh || { echo "Failed loading function library" >&2 ; exit 1 ; }
 
 # Parse the command options
 [ -r $RERUN_MODULES/@MODULE@/commands/@NAME@/options.sh ] && {
