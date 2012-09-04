60216cd5cbfa44f6cc9c867e5c1eb131db1dcf2c
Alex Honor <alexhonor@gmail.com>
2012-09-04 09:37:40 -0700

    added documentation comments

--- bash_completion.sh
+++ bash_completion.sh
@@ -1,8 +1,28 @@
 
 #
-# BASH shell tab completion for RERUN
+# rerun command completion script.
+#  **rerun** is a simple, small modular automation
+#  framework based on Bash, the POSIX shell.
 #
-# Source this file from your login shell. 
+# Installation
+# -------------
+#
+# #. Download or git clone from [rerun](http://github.com/rerun/rerun).
+# #. Source this file from your .bashrc.
+#
+# Usage
+# -----
+#
+# The contained completion support provides for:
+#
+# * Module listing:
+#       `rerun [tab][tab]`
+# * Command listing for specified module:
+#       `rerun module:[tab][tab]`
+# * Option listing for specified module:command:
+#       `rerun module: command[tab][tab]`
+# * Arguments for specified option:
+#       `rerun module: command --file[tab][tab]`
 #
 # @author: <a href="mailto:alex@dtosolutions.com">alex@dtosolutions.com</a>
 
@@ -10,42 +30,46 @@
     export RERUN_MODULES=$(pwd)/modules
 }
 
-# check if item is in the list
-list:exists()
+#
+# Shell functions to support the command completion
+#
+
+# list:member - check if item is contained in list
+list:member()
 {
     local  item="$1" list="$2"
-    for e in $(eval echo $list)
+    for member in $(eval echo $list)
     do
-	    [ "${item}" = "${e}" ] && return 0
+	    [ "${item}" = "${member}" ] && return 0
     done
     return 1	
 }
 
-# subtract the items in list2 from list1
+# list:subtract - subtract list2 members from list1
 list:subtract() 
 {
     local list1="$1" list2="$2" retlist=""
     for item in $(eval echo $list1)
     do
-	    list:exists $item "$list2" || retlist="$retlist $item"
+	    list:member $item "$list2" || retlist="$retlist $item"
     done
     echo $retlist    
 }
 
 
-# list all rerun modules
+# rerun:modules - list all rerun modules
 rerun:modules()
 {
     local  modules=""
     for mod in $RERUN_MODULES/*
     do 
-	    [ -d "$mod" -a -x "$mod" ] && modules="$modules $(basename $mod)"
+	    [ -f "$mod/metadata" ] && modules="$modules $(basename $mod)"
     done
     echo $modules
 }
 
 
-# list all the commands for the module
+# rerun:module:list - list all the commands for the module
 rerun:module:commands()
 {
     local  module=$1 commands=""
@@ -58,11 +82,10 @@ rerun:module:commands()
     echo $commands
 }
 
-# List all the registered options for the command
+# rerun:command:options - List all the registered options for the command
 rerun:command:options() 
 {
     local module=$1 command=$2 prefix=$3 options=""
-
     for opt in $RERUN_MODULES/$module/commands/$command/*.option; do
 	[ -f $opt ] && {
 		name=$(basename ${opt})
@@ -71,22 +94,17 @@ rerun:command:options()
     done
     echo $options
 }
-# Check if command has option
-rerun:command:has-option() 
-{
-    local module=$1 command=$2 option=$3
-    [ -f "$RERUN_MODULES/$module/commands/$command/${option##*-}.option" ] && return 0
-    return 1
-}
-# get the default for the specified option
+
+# rerun:option:default - get the default for the specified option
 rerun:option:default()
 {
     local module=$1 command=$2 opt=$3 
-	[ -f $RERUN_MODULES/$module/commands/$command/${opt##*-}.option ] && {
-    	awk -F= '/^DEFAULT/ {print $2}' $RERUN_MODULES/$module/commands/$command/${opt##*-}.option
+    [ -f $RERUN_MODULES/$module/commands/$command/${opt##*-}.option ] && {
+        awk -F= '/^DEFAULT/ {print $2}' $RERUN_MODULES/$module/commands/$command/${opt##*-}.option
 	}
 }
-# check if option takes an argument
+
+# rerun:option:has-argument - check if option takes an argument
 rerun:option:has-argument()
 {
     local module=$1 command=$2 opt=$3
@@ -94,7 +112,7 @@ rerun:option:has-argument()
     [ "$args" = "true" ] && return 0 || return 1
 }
 
-# list remaining options
+# rerun:options:remaining - list remaining options
 rerun:options:remaining() 
 {
     local argline=$1 options=$2 used="" 
@@ -104,10 +122,12 @@ rerun:options:remaining()
     list:subtract "$options" "$used"
 }
 
-# program completion for the 'rerun' command.
+#
+# _rerun - program completion for the `rerun` command.
+#
 _rerun() {
     [ -z "${RERUN_MODULES}" -o ! \( -d "${RERUN_MODULES}" \) ] && { 
-		return 0 ; 
+        return 0 ; 
     }
     local cur prev cntx_module cntx_command cntx_options options
     COMPREPLY=()
@@ -117,48 +137,50 @@ _rerun() {
     eval set $COMP_LINE
     shift; # shift once to drop the "rerun" from the argument string
 
-	# Define regex pattern to parse command line input
+    # Define regex pattern to parse command line input
     #   module:command --optionA arg --optionB arg ...
-    #
 	regex='([^:]+)([:]?[ ]?)([A-Za-z0-9_-]*)([ ]*)(.*)'
 	if [[ "$@" =~ $regex ]]
 	then
+        # module context
         [ -d "$RERUN_MODULES/${BASH_REMATCH[1]}" ] && cntx_module=${BASH_REMATCH[1]};
-		[ "${BASH_REMATCH[2]}" == ': ' ] && shift ;# eat the extra space char
+        [ "${BASH_REMATCH[2]}" == ': ' ] && shift ;# eat the extra space char
+        # command context
 	    [ -d "$RERUN_MODULES/$cntx_module/commands/${BASH_REMATCH[3]/ /}" ] && {
 	    	cntx_command=${BASH_REMATCH[3]/ /}
-		}
-		# BASH_REMATCH[4] contains the whitespace between command and options
-
-        # BASH_REMATCH[5] contains options
+        }
+        # BASH_REMATCH[4] contains the whitespace between command and options
+        # option context
         cntx_options=${BASH_REMATCH[5]};  
 	fi
 
     # Shift over to the command options
     shift;
 
-    # Empty context: list modules
+    # Complete commands given the user shell input. 
+
+    # Just the rerun command was typed. List modules
     [ -z "$cntx_module" ]  && {
-		local modules=$(rerun:modules $RERUN_MODULES)
+        local modules=$(rerun:modules $RERUN_MODULES)
         COMPREPLY=( $(compgen -W "$modules" -S ':' -o nospace -- ${cur}) )
         return 0
     }    
     
-    # Module context: list commands
+    # Module specified: List module's commands
     [ -n "$cntx_module" -a -z "$cntx_command" ] && {
-		local commands=$(rerun:module:commands ${cntx_module})
+        local commands=$(rerun:module:commands ${cntx_module})
         COMPREPLY=( $(compgen -W "$commands" -- ${cur}) )
         return 0
     }
     
-    # Command context. list options
+    # Command specified. List command's options
     options=$(rerun:command:options ${cntx_module} ${cntx_command} "--")
-
     if [ -n "$cntx_command" -a -z "$cntx_options"  ]; then
         COMPREPLY=( $(compgen -W "$options" -- "${cur}") )
         return 0 
     fi
 
+    # Option(s) specified. Show possible arguments or remaining option choices.
     if [ -n "$cntx_options"  ]; then
         if [[ $prev == -* ]]; then
             # check if current option takes an argument ...
@@ -166,16 +188,20 @@ _rerun() {
                 # ... and has a default value
                 local default=$(rerun:option:default ${cntx_module} ${cntx_command} ${prev})
                 if [ -n "$default" ]; then
+                    # print the default value
                     COMPREPLY=( $(compgen -W "$default" -- ${cur}) )
                     return 0
                 else
                     # ... or wants option specific completion
                     case "$prev" in
                         --file*|--out*|--xml|--template)
+                            # file completion
                 	        COMPREPLY=( $(compgen -o filenames -A file -- ${cur}) )  ;;
-                        --dir*|--logs*)
+                        --*dir*|--logs*)
+                            # directory completion
                 	        COMPREPLY=( $(compgen -o dirnames -A directory -- ${cur}) ) ;;
                         --module)
+                            # module completion
                             modules=$(rerun:modules)
                             COMPREPLY=( $(compgen -W "$modules" -- ${cur}) ) ;;
                     esac
@@ -185,15 +211,15 @@ _rerun() {
         else
         	# Show the remaining/unused option choices
             remaining=$(rerun:options:remaining "$cntx_options" "$options")
-
             COMPREPLY=( $(compgen -W "$remaining" -- ${cur}) )
 
 	    fi
 	    return 0
     fi
-
-
-
 }
 # register the _rerun completion function
 complete -F _rerun rerun
+
+#
+# This is Free Software distributed under the Apache 2 license.
+:
