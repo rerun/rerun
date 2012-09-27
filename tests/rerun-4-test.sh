d1a22c4aa415f5ddaac2a58e201e3de4fd876e93
Alex Honor <alexhonor@gmail.com>
2012-09-27 16:43:22 -0700

    Added new flag called "--answers <>" that lets users specify option values from a file. Fixes #23 Also changed execution logic to exec command scripts if they are executable. Fixes #21

---
+++ tests/rerun-4-test.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env roundup
+#
+
+# Let's get started
+# -----------------
+unset RERUN_COLORS
+unset RERUN_MODULES
+
+# Helpers
+# ------------
+. ./functions.sh || { echo "Failed loading test functions" ; exit 2 ; }
+
+rerun() {
+    command $RERUN "$@"
+}
+
+
+# The Plan
+# --------
+
+describe "rerun(4) Use an answer file"
+
+it_fails_with_missing_answers_file() {
+    # Check for expected error message.
+    rerun --answers 2>&1 | \
+        grep "SYNTAX"
+
+    # Check for expected error message.
+    rerun --answers foo 2>&1 | \
+        grep "ERROR: answers file not found: foo"
+}
+
+it_overrides_option_default() {
+    modules=$(mktemp -d "/tmp/rerun.modules.XXXXX")
+    OUT=$modules/out.txt
+    cat >$modules/answers  <<EOF
+JUMPS=$$
+EOF
+
+    make_freddy $modules
+    rerun --answers $modules/answers -M $modules freddy:dance > $OUT
+    head -1 $OUT | grep -q "jumps ($$)" $OUT
+    rm $OUT
+}
+
+it_ignores_unrecognized_answers() {
+    modules=$(mktemp -d "/tmp/rerun.modules.XXXXX")
+    OUT=$modules/out.txt
+    cat >$modules/answers  <<EOF
+BOGUS=blar
+PHONY=ralb
+EOF
+
+    make_freddy $modules
+    rerun --answers $modules/answers -M $modules freddy:dance > $OUT
+    head -1 $OUT | grep -q "jumps (3)" $OUT
+    rm $OUT
+}
+
