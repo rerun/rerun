a33f2eefcb6f6536a409c847b7fa2f5596cf7d96
Alex Honor <alexhonor@gmail.com>
2012-10-31 20:51:34 -0700

    Renamed prefix for stubbs functions to begin with stubbs.

--- modules/stubbs/lib/functions.sh
+++ modules/stubbs/lib/functions.sh
@@ -9,81 +9,81 @@
 #
 
 # Bootstrap a command handler
-rerun_init() {
+stubbs_init() {
     # Shell modules reside in current directory, by default. 
     homedir=$(dirname .)
     # Use env var property if it exists otherwise set it to default.
     [ -n "$RERUN_MODULES" ] || RERUN_MODULES=$homedir/modules    
 }
 
-rerun_optionArguments() {
+stubbs_optionArguments() {
     echo $(rerun_optionGetMetadataValue "$1/$2" $3 ARGUMENTS)
 }
 
-rerun_optionDefault() {
+stubbs_optionDefault() {
     echo $(rerun_optionGetMetadataValue "$1/$2" $3 DEFAULT)
 }
 
-rerun_optionShort() {
+stubbs_optionShort() {
     echo $(rerun_optionGetMetadataValue "$1/$2" $3 SHORT)
 }
 
-rerun_optionRequired() {
+stubbs_optionRequired() {
     echo $(rerun_optionGetMetadataValue "$1/$2" $3 REQUIRED)
 }
-rerun_optionExported() {
+stubbs_optionExported() {
     echo $(rerun_optionGetMetadataValue "$1/$2" $3 EXPORT)
 }
 
-rerun_commandDescription() {
+stubbs_commandDescription() {
     echo $(rerun_commandGetMetadataValue "$1/$2" $3 DESCRIPTION)
 }
 
-optionsWithDefaults() {
+stubbs_optionsWithDefaults() {
     local moddir=$1 module=$2 command=$3
 
     local optionsWithDefaults=""
     for opt in $(rerun_options $moddir $module $command); do
-        default=$(rerun_optionDefault $moddir $module $opt)
-        args=$(rerun_optionArguments $moddir $module $opt)
+        default=$(stubbs_optionDefault $moddir $module $opt)
+        args=$(stubbs_optionArguments $moddir $module $opt)
         [ -n "$default" -a "$args" == "true" ] && optionsWithDefaults="$optionsWithDefaults $opt"
     done
     echo $optionsWithDefaults
 }
 
 # list the options that are required
-optionsRequired() {
+stubbs_optionsRequired() {
     local moddir=$1 module=$2 command=$3
     local optionsRequired=""
 
     for opt in $(rerun_options $moddir $module $command); do
-        required=$(rerun_optionRequired $moddir $module $opt)
-        args=$(rerun_optionArguments $moddir $module $opt)
+        required=$(stubbs_optionRequired $moddir $module $opt)
+        args=$(stubbs_optionArguments $moddir $module $opt)
         [ "$required" == "true" -a "$args" = "true" ] && optionsRequired="$optionsRequired $opt"
     done
     echo $optionsRequired
 }
 # list the options that are exported as environment variables
-optionsExported() {
+stubbs_optionsExported() {
     local moddir=$1 module=$2 command=$3
     local optionsExported=""
 
     for opt in $(rerun_options $moddir $module $command); do
-        exported=$(rerun_optionExported $moddir $module $opt)
+        exported=$(stubbs_optionExported $moddir $module $opt)
         [ "$exported" == "true" ] && optionsExported="$optionsExported $opt"
     done
     echo $optionsExported
 }
 
 # Upper case the string and change dashes to underscores.
-trops() { echo "$1" | tr '[:lower:]' '[:upper:]' | tr  '-' '_' ; }
+stubbs_trops() { echo "$1" | tr '[:lower:]' '[:upper:]' | tr  '-' '_' ; }
 
 # Used to generate an entry inside options.sh
-add_optionparser() {
+stubbs_add_optionparser() {
 	local optName=$1
-    local optVarname=$(trops $optName)
-	local ARGUMENTS=$(rerun_optionArguments $moddir $module $optName)
-	local SHORT=$(rerun_optionShort $moddir $module $optName)
+    local optVarname=$(stubbs_trops $optName)
+	local ARGUMENTS=$(stubbs_optionArguments $moddir $module $optName)
+	local SHORT=$(stubbs_optionShort $moddir $module $optName)
 	if [ -n "${SHORT}" ] 
 	then
 		argstring=$(printf ' --%s|-%s' "${optName}"  "${SHORT}")
@@ -99,8 +99,8 @@ add_optionparser() {
 	fi
 }
 
-add_commandUsage() {
-    [ $# = 3 ] || { echo >&2 "usage add_commandUsage <moddir> <module> <command>" ; return 1 ; }
+stubbs_add_commandUsage() {
+    [ $# = 3 ] || { echo >&2 "usage stubbs_add_commandUsage <moddir> <module> <command>" ; return 1 ; }
     local moddir=$1 module=$2 command=$3
 
     for opt in $(rerun_options $moddir $module $command); do
@@ -127,28 +127,45 @@ add_commandUsage() {
     done
 }
 
-list_optionVariables() {
+stubbs_list_optionVariables() {
     [ $# = 3 ] || { 
-        echo >&2 "usage list_optionVariables <moddir> <module> <command>" 
+        echo >&2 "usage stubbs_list_optionVariables <moddir> <module> <command>" 
         return 1 ; 
     }
     local moddir=$1 module=$2 command=$3
     local summary=
     for option in $(rerun_options $moddir $module $command); do
-        summary="$summary $(trops $option)"
+        summary="$summary $(stubbs_trops $option)"
     done
     echo $summary
 }
 
-rerun_rewriteCommandScriptHeader() {
+stubbs_list_CommandOptionAssignments() {
+    [ $# = 2 ] || { 
+        echo >&2 "usage stubbs_list_CommandOptionAssignments <moddir> <option>" 
+        return 1 ; 
+    }
+    moddir=$1 option=$2
+    commands=()
+    for cmd_dir in $moddir/commands/*
+    do
+        OPTIONS=( $(. $cmd_dir/metadata ; echo $OPTIONS ) )
+        rerun_containsElement "$option" "${OPTIONS[*]}" && {
+            commands=( ${commands[*]} $(basename $cmd_dir) )
+        }
+    done
+    echo "${commands[*]}"
+}
+
+stubbs_rewriteCommandScriptHeader() {
     [ $# = 3 ] || { 
-        echo >&2 "usage rerun_rewriteCommandScriptHeader <moddir> <module> <command>" 
+        echo >&2 "usage stubbs_rewriteCommandScriptHeader <moddir> <module> <command>" 
         return 1 ; 
     }
     local moddir=$1 module=$2 command=$3
-    local desc=$(rerun_commandDescription $moddir $module $command)
-    local variables=$(list_optionVariables $moddir $module $command) || rerun_die
-    local usage=$(add_commandUsage $moddir $module $command) || rerun_die
+    local desc=$(stubbs_commandDescription $moddir $module $command)
+    local variables=$(stubbs_list_optionVariables $moddir $module $command) || rerun_die
+    local usage=$(stubbs_add_commandUsage $moddir $module $command) || rerun_die
     local RERUN_COMMAND_SCRIPT=$(rerun_moduleGetMetadataValue $moddir RERUN_COMMAND_SCRIPT)
     local commandScript=$moddir/$module/commands/$command/${RERUN_COMMAND_SCRIPT:-script}
     [ ! -f "$commandScript" ] && {
@@ -162,7 +179,7 @@ rerun_rewriteCommandScriptHeader() {
     # generated to stdout
 }
 
-rerun_rewriteCommandOptionsMetadata() {
+stubbs_rewriteCommandOptionsMetadata() {
     [ $# = 4 ] || { 
         echo >&2 "usage rerun_rewriteCommandOptionsMetadata <moddir> <module> <command> <options>" 
         return 1 ; 
