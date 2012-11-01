31c56047dadb33d1dbb8c9695d28e821e1f94616
Alex Honor <alexhonor@gmail.com>
2012-11-01 13:53:55 -0700

    Improve option processing tests.
    See #37

--- modules/stubbs/tests/rm-option-1-test.sh
+++ modules/stubbs/tests/rm-option-1-test.sh
@@ -6,6 +6,7 @@
 
 # Helpers
 # -------
+. $RERUN || exit 1 ;
 
 # A helper to run rerun.
 rerun() {
@@ -14,10 +15,18 @@ rerun() {
 
 # Mock a command and create an option.
 before() {
+    mkdir -p $RERUN_MODULES/freddy
+    cat > $RERUN_MODULES/freddy/metadata <<EOF
+NAME=freddy
+DESCRIPTION="mock freddy module created by $(basename $0)"
+EOF
     mkdir -p $RERUN_MODULES/freddy/commands/dance
-    rerun stubbs:add-option --module freddy --command dance \
-        --option jumps --desc "number of times to jump" \
-        --default 1 --required false
+    cat > $RERUN_MODULES/freddy/commands/dance/metadata <<EOF
+NAME=dance
+DESCRIPTION="mock dance command created by $(basename $0)"
+EOF
+
+    return $?
 }
 
 # Remove the mock module directory.
@@ -25,15 +34,16 @@ after() {
     rm -r $RERUN_MODULES/freddy
 }
 
-# Check the option metadata file exists and 
-# the parser no longer contains the option.
 validate() {
-    ! test -f $RERUN_MODULES/freddy/commands/dance/jumps.option
+    # Check the option metadata file exists and 
+    # the parser no longer contains the option.
+    ! test -f $RERUN_MODULES/freddy/options/jumps/metadata
     parser=$RERUN_MODULES/freddy/commands/dance/options.sh
     test -f $parser
     ! grep -q "\-j\|\--jumps)" $parser 
-    ! grep -q '[ -z "$JUMPS" ] && JUMPS="3"' $parser
+    ! grep -q '[ -z "$JUMPS" ] && JUMPS="1"' $parser
     ! grep -q '"missing required option: --jumps"' $parser
+    return $?
 }
 
 # The Plan
@@ -43,15 +53,68 @@ describe "rm-option"
 
 # Run it in interactive mode.
 it_runs_interactively() {
+    # Add option "--jumps|-j <1>"
+    rerun stubbs:add-option --module freddy --command dance \
+        --option jumps --desc "number of times to jump" \
+        --default 1 --required false
+
     rerun stubbs:rm-option --module freddy --command dance<<EOF
 jumps
 EOF
-    validate
 
+    validate
 }
 
 # Run the command with all options.
 it_runs_fully_optioned() {
+    # Add option "--jumps|-j <1>"
+    rerun stubbs:add-option --module freddy --command dance \
+        --option jumps --desc "number of times to jump" \
+        --default 1 --required false
+
     rerun stubbs:rm-option --module freddy --command dance --option jumps
+
     validate
 }
+
+# Run the command with all options.
+it_removes_option_after_last_assignment() {
+    # Add option "--jumps|-j <1>"
+    rerun stubbs:add-option --module freddy --command dance \
+        --option jumps --desc "number of times to jump" \
+        --default 1 --required false
+    #
+    # Add a second option: "--height|-h <5>"
+    rerun stubbs:add-option --module freddy --command dance \
+        --option height --desc "height to jump" \
+        --default 5 --required false
+    #
+    # Check the command's option assignment
+    OPTIONS=( $(.  $RERUN_MODULES/freddy/commands/dance/metadata; echo $OPTIONS) )
+    test ${#OPTIONS[*]} = 2
+    rerun_containsElement "jumps" "${OPTIONS[@]}"
+    rerun_containsElement "height" "${OPTIONS[@]}"
+    #
+    # Remove --jumps
+    rerun stubbs:rm-option --module freddy --command dance --option jumps
+    #
+    # Check if the option has been unassigned.
+    OPTIONS=( $(.  $RERUN_MODULES/freddy/commands/dance/metadata; echo $OPTIONS) )
+    test ${#OPTIONS[*]} = 1
+    ! rerun_containsElement "jumps" "${OPTIONS[@]}"
+    rerun_containsElement "height" "${OPTIONS[@]}"
+    # Ensure that the option declaration was removed from the module
+    ! test -d $RERUN_MODULES/freddy/options/jumps
+    #
+    # Remove --height
+    rerun stubbs:rm-option --module freddy --command dance --option height
+    #
+    # Ensure the dance command has no option assignments
+    OPTIONS=( $(.  $RERUN_MODULES/freddy/commands/dance/metadata; echo $OPTIONS) )
+    test -z "$OPTIONS"
+    test ${#OPTIONS[*]} = 0
+    #
+    # Ensure that the option declaration was removed from the module
+    ! test -d $RERUN_MODULES/freddy/options/height
+}
+
