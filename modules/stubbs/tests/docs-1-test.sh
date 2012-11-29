5d4be8ebce98b5f69f45b8aa5507e9549501f7f0
Alex Honor <alexhonor@gmail.com>
2012-11-28 19:30:50 -0800

    added unit test for html docs
    remove noclobber shell opt from docs command.
    See #94

--- modules/stubbs/tests/docs-1-test.sh
+++ modules/stubbs/tests/docs-1-test.sh
@@ -11,7 +11,7 @@ rerun() {
     command $RERUN -M $RERUN_MODULES "$@"
 }
 
-validate() {
+validate_manpage() {
     doc=$1
     test -f $doc
 
@@ -20,6 +20,27 @@ validate() {
     grep -q '.SH NAME' $doc
     grep -q '.SH SYNOPSIS' $doc
     grep -q '.SH COMMANDS' $doc
+
+}
+
+validate_webpages() {
+    dir=$1
+    test -f $dir/index.html
+    test -f $dir/docs.css
+    test -d $dir/lib
+    test -d $dir/options
+    test -d $dir/commands
+    for cmd in $dir/commands/*
+    do
+        cmd_name=$(basename $cmd)
+        test -f $dir/commands/$cmd_name/index.html
+        test -f $dir/commands/$cmd_name/script.html
+    done
+    for opt in $dir/options/*
+    do
+        opt_name=$(basename $opt)
+        test -f $dir/options/$opt_name/index.html
+    done
 
 }
 
@@ -33,15 +54,17 @@ it_runs_interactively() {
     rerun stubbs:docs <<EOF
 stubbs
 EOF
-    validate $RERUN_MODULE_DIR/docs/stubbs.1
-
+    validate_manpage $RERUN_MODULE_DIR/docs/stubbs.1
+    validate_webpages $RERUN_MODULE_DIR/docs
 }
 
 it_runs_fully_optioned() {
     DIR=$(mktemp -d /tmp/stubbs.docs.XXXX)
-    rerun stubbs:docs --module "stubbs" --dir $DIR
+    rerun stubbs:docs --module "stubbs" --dir "$DIR"
     
-    validate $DIR/stubbs.1
-    rm -r $DIR
+    validate_manpage "$DIR/stubbs.1"
+    validate_webpages $DIR
+
+    rm -r "$DIR"
 }
 
