98479d080987df59302bcc1a7955d137c3e9bc4d
Alex Honor <alexhonor@yahoo.com>
2013-09-14 12:32:48 -0700

    refactor rerun_log function to use numeric levels internally to make code more efficient. Users still specify levels using symbolic names. Added more unit tests.
    Fixes #178

--- tests/rerun-0-test.sh
+++ tests/rerun-0-test.sh
@@ -270,11 +270,12 @@ it_performs_rerun_command_execute() {
 
 it_performs_rerun_log_puts() {
     . $RERUN
-
+    info_level=1
+    erro_level=$RERUN_LOG_ERR_LEVEL
     # Verify the default format is returned by fmt-console action.
     test "$RERUN_LOG_FMT_CONSOLE" = "$(rerun_log fmt-console)"
     # Print a message to the console
-    out=($(rerun_log_puts console info freddy:dance "hi there"))
+    out=($(rerun_log_puts console $info_level freddy:dance "hi there"))
     test "4" = "${#out[*]}"        
     test "[info]" = "${out[0]}"
     test "freddy:dance:" = "${out[1]}"
@@ -283,15 +284,15 @@ it_performs_rerun_log_puts() {
     test "[info] freddy:dance: hi there" = "${out[*]}"
 
     # Print an error level message and be sure it's not written to stdout.
-    test -z "$(rerun_log_puts console error freddy:dance "oh no, an error")"
+    test -z "$(rerun_log_puts console $erro_level freddy:dance "oh no, an error")"
     # Print the message and redirect stderr to capture the output
-    out=($(rerun_log_puts console error freddy:dance "oh no, an error" 2>&1))
+    out=($(rerun_log_puts console $erro_level freddy:dance "oh no, an error" 2>&1))
     test -n "$out"
 
     # Verify the default format is returned by fmt-logfile action.
     test "$RERUN_LOG_FMT_LOGFILE" = "$(rerun_log fmt-logfile)"
     # Write a message using logfile output.
-    out=($(rerun_log_puts logfile info freddy:dance "hi there"))
+    out=($(rerun_log_puts logfile $info_level freddy:dance "hi there"))
     test "5" = "${#out[*]}"        
     test "[info]" = "${out[1]}"
     test "freddy:dance:" = "${out[2]}"
@@ -299,12 +300,12 @@ it_performs_rerun_log_puts() {
     test "hi there" = "$message"
 
     # Be sure error message is is written to stderr and not stdout
-    test -z "$(rerun_log_puts logfile error freddy:dance "oh no, an error")"
-    out=($(rerun_log_puts logfile error freddy:dance "oh no, an error" 2>&1))
+    test -z "$(rerun_log_puts logfile $erro_level freddy:dance "oh no, an error")"
+    out=($(rerun_log_puts logfile $erro_level freddy:dance "oh no, an error" 2>&1))
     test -n "$out"    
 }
 
-it_performs_rerun_log() {
+it_performs_rerun_log_level() {
     . $RERUN
     # check the default log level
     test "info" = "$(rerun_log level)"
@@ -312,13 +313,66 @@ it_performs_rerun_log() {
     rerun_log level warn
     test "warn" = "$(rerun_log level)"
     ! rerun_log level "total crap"
+}
 
+it_performs_rerun_log_levels() {
+    . $RERUN
     # Get the log levels
     test -n "${RERUN_LOG_LEVELS[*]}"
     levels=($(rerun_log levels))
     test "${#levels[*]}" = "${#RERUN_LOG_LEVELS[*]}"
     test "${levels[*]}" = "${RERUN_LOG_LEVELS[*]}"
+}
 
+it_performs_rerun_log() {
+    . $RERUN
+    out=$(rerun_log "hi")
+    test "$out" = "[info] : hi"
+    out=$(rerun_log this is an unquoted message)
+    test "$out" = "[info] : this is an unquoted message"    
+}
+
+it_performs_rerun_log_debug() {
+    . $RERUN
+    rerun_log level debug
+    out=$(rerun_log debug "hi")
+    test "$out" = "[debug] : hi"
+}
+it_performs_rerun_log_info() {
+    . $RERUN
+    rerun_log level info
+    out=$(rerun_log info "hi")
+    test "$out" = "[info] : hi"
+}
+it_performs_rerun_log_warn() {
+    . $RERUN
+    rerun_log level warn
+    out=$(rerun_log warn "hi")
+    test "$out" = "[warn] : hi"
+}
+it_performs_rerun_log_error() {
+    . $RERUN
+    rerun_log level error
+    test -z "$(rerun_log error "hi")"
+    # set the err level threshold above fatals to write to stdout 
+    # and avoid getting roundup's verbose output into the stderr stream
+    RERUN_LOG_ERR_LEVEL=5
+    out=$(rerun_log error "hi")
+    test "$out" = "[error] : hi"
+}
+it_performs_rerun_log_fatal() {
+    . $RERUN
+    rerun_log level fatal
+    test -z "$(rerun_log fatal "hi")"
+    # set the err level threshold above fatals to write to stdout 
+    # and avoid getting roundup's verbose output into the stderr stream
+    RERUN_LOG_ERR_LEVEL=6
+    out=$(rerun_log fatal "hi")
+    test "$out" = "[fatal] : hi"
+}
+
+it_performs_rerun_log_logfile() {
+    . $RERUN
     # Get the configured logfile
     test -z "$(rerun_log logfile)"
     logfile=$(mktemp "/tmp/rerun.test.XXXXX")
@@ -335,8 +389,11 @@ it_performs_rerun_log() {
     rerun_log info "info message"
     test "2" = $(wc -l $logfile | awk '{print $1}')
     grep "info message" $logfile
+    rm "$logfile"
+}
 
-
+it_performs_rerun_log_with_command_context() {
+    . $RERUN
     RERUN_MODULE_DIR=$RERUN_MODULES/freddy
     RERUN_COMMAND_DIR=$RERUN_MODULE_DIR/commands/dance
     out=$(rerun_log info "freddy says hi")
@@ -344,11 +401,12 @@ it_performs_rerun_log() {
 
     out=$(rerun_log "freddy says hi")
     test "[info] freddy:dance: freddy says hi" = "$out"
+}
 
+it_performs_rerun_log_with_syslog() {
+    . $RERUN
     test -z $(rerun_log syslog)
     rerun_log syslog user 
     ! rerun_log syslog "total crap"
-
-
-    rm "$logfile"
+    rerun_log "test[#$$] it_performs_rerun_log_with_syslog"
 }
