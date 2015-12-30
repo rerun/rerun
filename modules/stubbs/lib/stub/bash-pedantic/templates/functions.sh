494dd0c3a6e4c866830d0698fc75f182f458a554
Alex Honor <alexhonor@yahoo.com>
2015-12-30 11:10:38 -0800

    Added template dir support for module stubs.
    Original one is now called bash-pedantic and new ones is more
    stripped down with less comments and error trap.

---
+++ modules/stubbs/lib/stub/bash-pedantic/templates/functions.sh
@@ -0,0 +1,28 @@
+# Shell functions for the @MODULE@ module.
+#/ usage: source RERUN_MODULE_DIR/lib/functions.sh command
+#
+
+# Read rerun's public functions
+. $RERUN || {
+    echo >&2 "ERROR: Failed sourcing rerun function library: \"$RERUN\""
+    return 1
+}
+
+# Check usage. Argument should be command name.
+[[ $# = 1 ]] || rerun_option_usage
+
+# Source the option parser script.
+#
+if [[ -r $RERUN_MODULE_DIR/commands/$1/options.sh ]] 
+then
+    . $RERUN_MODULE_DIR/commands/$1/options.sh || {
+        rerun_die "Failed loading options parser."
+    }
+fi
+
+# - - -
+# Your functions declared here.
+# - - -
+
+
+
