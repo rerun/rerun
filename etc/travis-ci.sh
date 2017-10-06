1efda0aea2ac04da4da9b022fa014b8d550aef31
Lee Thompson <lthompson@alienvault.com>
2017-10-06 08:18:30 -0500

    GH-198 attempt to set version number to build

--- etc/travis-ci.sh
+++ etc/travis-ci.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Continuous Integration at https://travis-ci.org/rerun/rerun
+
+# set version patch number to build number from travis
+sed -i -r 's,^RERUN_VERSION=([0-9]+\.[0-9]+)\.0$,RERUN_VERSION=\1.'"${TRAVIS_BUILD_NUMBER}"',g' rerun
+grep '^RERUN_VERSION=' rerun
+
 autoreconf --install
 ./configure
 make check
