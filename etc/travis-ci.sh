473c14806bf0c295c56f6c5105118b31b4bec6b0
Lee Thompson <stagr.lee@gmail.com>
2017-10-09 18:47:08 -0500

    GH-198 updating travis job to create bins (#251)
    * GH-198 wiring travis up to bintray

--- etc/travis-ci.sh
+++ etc/travis-ci.sh
@@ -1,4 +1,24 @@
+#!/bin/bash
+set -e
+
 # Continuous Integration at https://travis-ci.org/rerun/rerun
+
+# set version patch number to build number from travis
+sed -i -r 's,^RERUN_VERSION=([0-9]+\.[0-9]+)\.0$,RERUN_VERSION=\1.'"${TRAVIS_BUILD_NUMBER}"',g' rerun
+grep '^RERUN_VERSION=' rerun
+
 autoreconf --install
 ./configure
-make check
+mkdir -p tmp
+make DESTDIR=./tmp install
+make distcheck
+
+# make the distributions
+make bin deb rpm
+
+if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+  # Time to push to bintray
+  make release
+else
+  echo "Travis-CI sayz LGTM"
+fi
