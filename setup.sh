6a6247fcb749e613a6dd4591f3606938b24b6ef2
Lee Thompson <stagr.lee@gmail.com>
2017-10-10 13:24:59 -0500

    GH-198 more fixes (#253)
    * GH-198 starting work on travis binary release to bintray
    
    * GH-198 no install on this build, use just gcc
    
    * GH-198 maybe spaces is borking build
    
    * GH-198 having trouble with addon syntax
    
    * GH-198 add fakeroot debian package
    
    * GH-198 attempt to set version number to build
    
    * GH-198 attempt to make the distribution
    
    * GH-198 define LOGNAME when undefined in a travis deb test
    
    * GH-198 dh_make --yes
    
    * GH-198 define LOGNAME early
    
    * GH-198 export LOGNAME
    
    * GH-198 debugging rpm build
    
    * GH-198 debugging rpm build, seems to work
    
    * GH-198 Add artifact pushes
    
    * GH-198 fix rpm tests on ubuntu
    
    * GH-198 fix rpm tests on ubuntu
    
    * GH-198 fix rpm tests on ubuntu
    
    * GH-198 fix rpm tests on ubuntu
    
    * GH-198 fought with automake syntax to build/ship system packages

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ export LC_ALL=C
 RERUN_VERSION=$(grep ^RERUN_VERSION rerun  | cut -d= -f2)
 
 # RUN ME after pulling the code from git!
-if [ "clean" == "$1" ]; then
+if [[ "clean" == "$1" ]]; then
   if [[ -e Makefile ]]
   then
     make distclean
@@ -14,12 +14,17 @@ if [ "clean" == "$1" ]; then
   if [[ -d rerun-${RERUN_VERSION} ]]
   then
     find rerun-${RERUN_VERSION} -type d -exec chmod 755 {} \;
-    rm -rf tmp rerun-* autom4te.cache config
   fi
 
-  rm -f INSTALL ChangeLog Makefile.in aclocal.m4 configure rerun-${RERUN_VERSION}* rerun-stubbs-*.rpm
+  rm -f INSTALL ChangeLog Makefile.in aclocal.m4 configure rerun-stubbs*
   rm -f rerun_${RERUN_VERSION}*
+  rm -rf tmp rerun[-_]${RERUN_VERSION}* autom4te.cache config
   rm -rf rpm autom4te.cache config tmp deb
+elif [[ "regress" == "$1" ]]; then
+  ./setup.sh clean
+  ./setup.sh
+  make bin rpm deb
+  make release
 else
   autoreconf --install
   ./configure
